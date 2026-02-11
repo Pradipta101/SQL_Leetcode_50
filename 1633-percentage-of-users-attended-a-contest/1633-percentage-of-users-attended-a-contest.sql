1b222cc0b0a1d5751ea3f7edb3cafe99b784f8fe
Pradipta Singh <spradipta702@gmail.com>
2026-02-12 00:17:09 +0530

    Time: 948 ms (50.67%), Space: 0B (100%) - LeetHub

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -10,11 +10,10 @@ CROSS JOIN (
 GROUP BY r.contest_id
 ORDER BY percentage DESC, r.contest_id ASC;
 
-another way
-select r.contest_id ,
+/*select r.contest_id ,
 round(count(distinct u.user_id)*100/( select count(user_id)from users) ,2)as percentage
 from users u
 join register r
 on u.user_id = r.user_id
 group by r.contest_id
-order by percentage desc, r.contest_id
+order by percentage desc, r.contest_id*/
