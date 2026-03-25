525c881b53d1fb40e88d726d8547f647a6a72add
Pradipta Singh <spradipta702@gmail.com>
2026-03-25 19:56:31 +0530

    Time: 560 ms (33.16%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,10 +1,13 @@
 # Write your MySQL query statement below
-select employee_id, department_id 
-from employee 
+-- Employees with primary department
+select employee_id, department_id
+from employee
 where primary_flag = 'Y'
+
+union
+
+-- Employees with only one department
+select employee_id, max(department_id) as department_id
+from employee
 group by employee_id
-Union
-select employee_id, department_id 
-from employee 
-group by employee_id 
-having count(employee_id) = 1
+having count(*) = 1
