124830ff0fa79974ba8e7d5595c4551b9743cdcd
Pradipta Singh <spradipta702@gmail.com>
2026-04-11 19:24:45 +0530

    Time: 288 ms (24.01%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,5 +1,14 @@
 # Write your MySQL query statement below
 
-SELECT MAX(e1.salary) AS SecondHighestSalary
+/*SELECT MAX(e1.salary) AS SecondHighestSalary
 FROM Employee  e1 inner join employee e2 
-on e1.salary < e2.salary
+on e1.salary < e2.salary*/
+
+WITH ranked AS (
+    SELECT salary,
+           DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
+    FROM Employee
+)
+SELECT max(salary) AS SecondHighestSalary
+FROM ranked
+where rnk = 2
